34b90a43f0947f096e7d5f827102bf66cf8cea1c
wolves <cstingl@protonmail.com>
2022-11-28 13:32:47 -0500

    Update hyprland default - remove portal-gtk

--- modules/desktop/hyprland/default.nix
+++ modules/desktop/hyprland/default.nix
@@ -3,20 +3,30 @@
 {
   imports = [ ../../programs/waybar.nix ];
 
+  services.dbus.enable = true;
+
   environment = {
     loginShellInit = ''
       if [ -z $DISPLAY ] && [ "$(tty)" = "/dev/tty1" ]; then
         exec Hyprland
       fi
     '';
+    variables = {
+      XDG_CURRENT_DESKTOP="Hyprland";
+      XDG_SESSION_TYPE="wayland";
+      XDG_SESSION_DESKTOP="Hyprland";
+    };
+    systemPackages = with pkgs; [
+      grim
+      #mpvpaper
+      slurp
+      swappy
+      wl-clipboard
+      wlr-randr
+    ];
   };
 
   programs = {
     hyprland.enable = true;
-  };
-
-  xdg.portal = {
-    enable = true;
-    extraPortals = [ pkgs.xdg-desktop-portal-gtk ];
   };
 }
