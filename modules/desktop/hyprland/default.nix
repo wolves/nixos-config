ab39030d8586ed397031b616ecd2258cd2c2b17c
wolves <cstingl@protonmail.com>
2022-11-28 12:41:07 -0500

    Uncomment hyprland waybar import & fix typo

--- modules/desktop/hyprland/default.nix
+++ modules/desktop/hyprland/default.nix
@@ -1,7 +1,7 @@
-{ config, lib, pkgs, ... }
+{ config, lib, pkgs, ... }:
 
 {
-  #imports = [ ../../programs/waybar.nix ];
+  imports = [ ../../programs/waybar.nix ];
 
   environment = {
     loginShellInit = ''
