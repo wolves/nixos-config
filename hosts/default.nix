7e7d51cc1537c0e1e3ac7e0b09bdd891425ec1d0
wolves <cstingl@protonmail.com>
2022-11-29 16:02:52 -0500

    Nest home-manager args

--- hosts/default.nix
+++ hosts/default.nix
@@ -20,11 +20,13 @@ in
       ./configuration.nix
 
       home-manager.nixosModules.home-manager {
-        home-manager.useGlobalPkgs = true;
-        home-manager.useUserPackages = true;
-        home-manager.extraSpecialArgs = { inherit user; };
-        home-manager.users.${user} = {
-          imports = [(./home.nix)] ++ [(import ./laptop/home.nix)];
+        home-manager = {
+          useGlobalPkgs = true;
+          useUserPackages = true;
+          extraSpecialArgs = { inherit user; };
+          users.${user} = {
+            imports = [(./home.nix)] ++ [(import ./laptop/home.nix)];
+          };
         };
       }
     ];
