a831dd934db7e287c61ef566cf4f45a7c24f7781
wolves <cstingl@protonmail.com>
2022-11-29 11:06:10 -0500

    Font config updates

--- hosts/configuration.nix
+++ hosts/configuration.nix
@@ -25,26 +25,46 @@
 
   security.rtkit.enable = true;
 
-  fonts.fonts = with pkgs; [
-    material-icons
-    material-design-icons
-    carlito
-    vegur
-    inter
-    iosevka-bin
-    lato
-    source-code-pro
-    jetbrains-mono
-    font-awesome
-    corefonts
-    (nerdfonts.override {
-      fonts = [
-        "Iosevka"
-        "JetBrainsMono"
-        "FiraCode"
-      ];
-    })
-  ];
+  fonts = {
+    fonts = with pkgs; [
+      material-icons
+      material-design-icons
+      carlito
+      vegur
+      inter
+      iosevka-bin
+      lato
+      source-code-pro
+      noto-fonts
+      noto-fonts-emoji
+      font-awesome
+      jetbrains-mono
+      corefonts
+      (nerdfonts.override {
+        fonts = [
+          "Iosevka"
+          "JetBrainsMono"
+          "FiraCode"
+        ];
+      })
+    ];
+
+    enableDefaultFonts = false;
+
+    fontConfig = {
+      defaultFonts ={
+        monospace = [
+          "Iosevka Term"
+          "Iosevka Term Nerd Font Complete Mono"
+          "Iosevka Nerd Font"
+          "Noto Color Emoji"
+        ];
+        sansSerif = ["Noto Sans" "Noto Color Emoji"];
+        serif = ["Noto Serif" "Noto Color Emoji"];
+        emoji = ["Noto Color Emoji"];
+      };
+    };
+  };
 
   environment = {
     variables = {
