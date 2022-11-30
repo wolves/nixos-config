f44668ea9e7bdd764aaf0e28bde1eb569294e245
wolves <cstingl@protonmail.com>
2022-11-30 12:51:39 -0500

    Cleanout temp nvim settings

--- modules/editors/nvim/default.nix
+++ modules/editors/nvim/default.nix
@@ -5,29 +5,9 @@
 { pkgs, ... }:
 
 {
-  programs = {
-    neovim = {
-      enable = true;
-      viAlias = true;
-      vimAlias = true;
-
-      plugins = with pkgs.vimPlugins; [
-	# Syntax
-	vim-nix
-
-	# Customization
-	srcery-vim
-      ];
-
-      extraConfig = ''
-	syntax enable
-	colorscheme srcery
-	
-	highlight Comment cterm=italic gui=italic
-	
-        set number
-        set relativenumber
-      '';
-    };
+  programs.neovim = {
+    enable = true;
+    viAlias = true;
+    vimAlias = true;
   };
 }
