ce7869f78e62e03749ce2f116276360c34689984
wolves <cstingl@protonmail.com>
2022-11-30 18:59:46 -0500

    Update alacritty/tmux config

--- modules/programs/alacritty.nix
+++ modules/programs/alacritty.nix
@@ -5,6 +5,8 @@
     alacritty = {
       enable = true;
       settings = {
+        env.TERM = "xterm-256color";
+        shell.program = "${pkgs.tmux}/bin/tmux";
         font = rec {
           #normal.family = "JetBrains Mono";
           #normal.family = "Source Code Pro";
