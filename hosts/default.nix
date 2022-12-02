d3f80517ae5f91a7924b6a11c56e8c09800eea24
wolves <cstingl@protonmail.com>
2022-12-02 13:25:08 -0500

    Cleanout commented lines

--- hosts/default.nix
+++ hosts/default.nix
@@ -20,49 +20,20 @@
       inherit self;
     };
     users.wlvs = ../modules/home;
-    # users.wlvs = {
-    #   imports = [(./home.nix)] ++ [(import ./laptop/home.nix)];
-    # };
   };
-  # pkgs = import nixpkgs {
-  #   inherit system;
-  #   config.allowUnfree = true;
-  # };
-  #
-  # lib = nixpkgs.lib;
 in {
   laptop = nixpkgs.lib.nixosSystem {
-    # inherit system;
     system = "x86_64-linux";
-    # specialArgs = { inherit inputs user location hyprland; };
     modules = [
       {networking.hostName = "fwrk";}
       ./laptop/hardware-configuration.nix
-
-      ./laptop
       bootloader
-      # hw.framework
+      hw.framework
       wayland
       hmModule
       {inherit home-manager;}
-
-      # inputs.hyprland.nixosModules.default
-    ] ++
-    shared;
-
-      # home-manager.nixosModules.home-manager {
-      #   home-manager = {
-      #     useGlobalPkgs = true;
-      #     useUserPackages = true;
-      #     extraSpecialArgs = { inherit inputs user; };
-      #     users.${user} = {
-      #       imports = [(./home.nix)] ++ [(import ./laptop/home.nix)];
-      #     };
-      #   };
-      # }
-    # ] ++
-    # shared;
-
-    specialArgs = { inherit inputs; };
+    ]
+    ++ shared;
+  specialArgs = { inherit inputs; };
   };
 }
