8fc6b648b3664a3ea56440372874282a994fdecb
wolves <cstingl@protonmail.com>
2022-12-15 07:49:42 -0500

    Adding pam security wayland unlock text

--- modules/core/security.nix
+++ modules/core/security.nix
@@ -5,6 +5,10 @@
 }: {
   security = {
     sudo.wheelNeedsPassword = false;
+
+    # allow wayland lockers to unlock the screen
+    pam.services.swaylock.text = "auth include login";
+
     rtkit.enable = true;
   };
 }
