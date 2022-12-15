74f16de20341fb77d80f448a33f5d4ac04413067
wolves <cstingl@protonmail.com>
2022-12-15 10:12:50 -0500

    Customize swaylock config, good enough for now

--- modules/home/swaylock/default.nix
+++ modules/home/swaylock/default.nix
@@ -13,21 +13,56 @@
     yellow = "#DCA561"; # Kanagawa AutumnYellow
     orange = "#FFA066"; # Kanagawa SurimiOrange
   in {
-    clock = true;
-    effect-blur = "30x3";
-    font = "Lato";
-    ignore-empty-password = true;
-    indicator = true;
     image = "$HOME/wallpaper/aenami_escape_1k.jpg";
-    bs-hl-color = "${red}";
-    key-hl-color = "${orange}";
-    inside-clear-color = "${red}";
-    inside-color = "${yellow}";
-    inside-ver-color = "${blue}";
-    inside-wrong-color = "${red}";
-    line-color = "${crust}";
-    ring-color = "${base}";
-    separator-color = "${sumiInk4}";
+    effect-blur = "30x3";
+
+    scaling = "fill";
+
+    indicator = true;
+    indicator-x-position = "930";
+    indicator-y-position = "515";
+    indicator-radius = "456";
+    clock = true;
+    ignore-empty-password = true;
+    disable-caps-lock-text = true;
+
+    timestr = "%I:%M %p";
+    datestr = "";
+
+    font = "Lato";
+    font-size = "52";
+    text-color = "${blue}";
+
+    text-ver-color = "00000000";
+    text-wrong-color = "00000000";
+    text-clear-color = "00000000";
+    inside-color = "00000000";
+    inside-ver-color = "00000000";
+    inside-wrong-color = "00000000";
+    inside-clear-color = "00000000";
+    inside-caps-lock-color = "00000000";
+    ring-color = "00000000";
+    ring-ver-color = "00000000";
+    ring-wrong-color = "00000000";
+    ring-clear-color = "00000000";
+    line-color = "00000000";
+    line-clear-color = "00000000";
+    line-ver-color = "00000000";
+    key-hl-color = "00000000";
+    bs-hl-color = "00000000";
+    caps-lock-bs-hl-color = "00000000";
+    caps-lock-key-hl-color = "00000000";
+    separator-color = "00000000";
+
+    # bs-hl-color = "${red}";
+    # key-hl-color = "${orange}";
+    # inside-clear-color = "${red}";
+    # inside-color = "${yellow}";
+    # inside-ver-color = "${blue}";
+    # inside-wrong-color = "${red}";
+    # line-color = "${crust}";
+    # ring-color = "${base}";
+    # separator-color = "${sumiInk4}";
   };
 }
 
