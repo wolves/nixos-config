224a6259dbbda29d47ec5ec5fb9fc98c046eafc4
wolves <cstingl@protonmail.com>
2022-12-15 12:55:41 -0500

    Sudo -> Doas

--- modules/core/security.nix
+++ modules/core/security.nix
@@ -4,14 +4,44 @@
   ...
 }: {
   security = {
-    sudo.wheelNeedsPassword = false;
+    rtkit.enable = true;
 
     pam.services = {
       login.enableGnomeKeyring = true;
       # allow wayland lockers to unlock the screen
       swaylock.text = "auth include login";
     };
-
-    rtkit.enable = true;
+    # sudo.wheelNeedsPassword = false;
+    doas = {
+      enable = true;
+      extraRules = [
+        {
+          groups = ["wheel"];
+          persist = true;
+          keepEnv = false;
+        }
+        {
+          groups = ["power"];
+          noPass = true;
+          cmd = "${pkgs.systemd}/bin/poweroff";
+        }
+        {
+          groups = ["power"];
+          noPass = true;
+          cmd = "${pkgs.systemd}/bin/reboot";
+        }
+        {
+          groups = ["nix"];
+          cmd = "nix-collect-garbage";
+          noPass = true;
+        }
+        {
+          groups = ["nix"];
+          cmd = "nixos-rebuild";
+          keepEnv = true;
+        }
+      ];
+    };
+    sudo.enable = false;
   };
 }
