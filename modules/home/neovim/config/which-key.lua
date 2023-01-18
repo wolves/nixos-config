415ef5e322a69201f68b2e361c019bd24a0fe7ba
wolves <cstingl@protonmail.com>
2023-01-17 19:37:26 -0500

    Move telescope binds and helper funcs into which-key config

--- modules/home/neovim/config/which-key.lua
+++ modules/home/neovim/config/which-key.lua
@@ -70,6 +70,47 @@ wk.register({
   ["<leader>x"] = { name = "+diagnostics/quickfix" },
 })
 
+-- Telescope: Helper Funcs
+local function grep_string_prompt()
+  require("telescope.builtin").grep_string({
+    path_display = { "shorten" },
+    search = vim.fn.input("Grep String ❱ "),
+  })
+end
+
+local function grep_word()
+  require("telescope.builtin").grep_string({
+    path_display = { "shorten" },
+    search = vim.fn.expand("<cword>"),
+  })
+end
+
+local function project_files(opts)
+  opts = opts or {}
+  opts.show_untracked = true
+  if vim.loop.fs_stat(".git") then
+    require("telescope.builtin").git_files(opts)
+  else
+    local client = vim.lsp.get_active_clients()[1]
+    if client then
+      opts.cwd = client.config.root_dir
+    end
+    require("telescope.builtin").find_files(opts)
+  end
+end
+--/ Telescope: Helper Funcs
+vim.keymap.set("n", "<leader><space>", function()
+  project_files()
+end, { desc = "Find File" })
+
+vim.keymap.set("n", "<leader>ss", function()
+  grep_string_prompt()
+end, { desc = "Grep Prompt" })
+
+vim.keymap.set("n", "<leader>sw", function()
+  grep_word()
+end, { desc = "Grep Current Word" })
+
 local leader = {
   w = { "<cmd>w!<CR>", "Save" },
   q = { "<cmd>q!<CR>", "Quit" },
