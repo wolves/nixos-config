0387b1996b079be2fcce981d86f155e9c71ca3fb
wolves <cstingl@protonmail.com>
2023-01-22 09:38:40 -0500

    Comment out TS playground and textobjects

--- modules/home/neovim/config/treesitter.lua
+++ modules/home/neovim/config/treesitter.lua
@@ -18,33 +18,33 @@ require("nvim-treesitter.configs").setup({
 		use_virtual_text = true,
 		lint_events = { "BufWrite", "CursorHold" },
 	},
-	playground = {
-		enable = true,
-		disable = {},
-		updatetime = 25, -- Debounced time for highlighting nodes in the playground from source code
-		persist_queries = true, -- Whether the query persists across vim sessions
-		keybindings = {
-			toggle_query_editor = "o",
-			toggle_hl_groups = "i",
-			toggle_injected_languages = "t",
-			toggle_anonymous_nodes = "a",
-			toggle_language_display = "I",
-			focus_language = "f",
-			unfocus_language = "F",
-			update = "R",
-			goto_node = "<cr>",
-			show_help = "?",
-		},
-	},
-	textobjects = {
-		select = {
-			enable = false,
-		},
-		move = {
-			enable = false,
-		},
-		lsp_interop = {
-			enable = false,
-		},
-	},
+	-- playground = {
+	-- 	enable = true,
+	-- 	disable = {},
+	-- 	updatetime = 25, -- Debounced time for highlighting nodes in the playground from source code
+	-- 	persist_queries = true, -- Whether the query persists across vim sessions
+	-- 	keybindings = {
+	-- 		toggle_query_editor = "o",
+	-- 		toggle_hl_groups = "i",
+	-- 		toggle_injected_languages = "t",
+	-- 		toggle_anonymous_nodes = "a",
+	-- 		toggle_language_display = "I",
+	-- 		focus_language = "f",
+	-- 		unfocus_language = "F",
+	-- 		update = "R",
+	-- 		goto_node = "<cr>",
+	-- 		show_help = "?",
+	-- 	},
+	-- },
+	-- textobjects = {
+	-- 	select = {
+	-- 		enable = false,
+	-- 	},
+	-- 	move = {
+	-- 		enable = false,
+	-- 	},
+	-- 	lsp_interop = {
+	-- 		enable = false,
+	-- 	},
+	-- },
 })
