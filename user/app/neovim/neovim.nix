3899b9c9d3c379330a354bb9d444963cb97d9faa
Wolves <cstingl@protonmail.com>
2023-12-08 13:39:52 -0500

    neovim: Add better escape plugin

--- user/app/neovim/neovim.nix
+++ user/app/neovim/neovim.nix
@@ -50,6 +50,18 @@
         type = "lua";
         config = luaConfig ./nvim/plugin/which-key.lua;
       }
+      {
+        plugin = better-escape-nvim;
+        type = "lua";
+        config = ''
+          require("better_escape").setup({
+            mapping = { "jk", "jj" }, -- a table with mappings to use
+            timeout = vim.o.timeoutlen, -- the time in which the keys must be hit in ms. Use option timeoutlen by default
+            clear_empty_lines = false, -- clear line after escaping if there is only whitespace
+            keys = "<Esc>", -- keys used for escaping, if it is a function will use the result everytime
+          })
+        '';
+      }
     ];
 
     extraLuaConfig = ''
