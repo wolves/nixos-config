3a100213ab0c551e495dc7a902a8789f7e8a5b02
Wolves <cstingl@protonmail.com>
2023-12-09 22:42:18 -0500

    neovim: cleanup/refactor better_escape config into editor config file

--- config/nvim/lua/wlvs/editor.lua
+++ config/nvim/lua/wlvs/editor.lua
@@ -1,4 +1,11 @@
 local function init()
+  require("better_escape").setup({
+    mapping = { "jk", "jj" }, -- a table with mappings to use
+    timeout = vim.o.timeoutlen, -- the time in which the keys must be hit in ms. Use option timeoutlen by default
+    clear_empty_lines = false, -- clear line after escaping if there is only whitespace
+    keys = "<Esc>", -- keys used for escaping, if it is a function will use the result everytime
+  })
+
   require("colorizer").setup({
     filetypes = { "*", "!lazy" },
     buftype = { "*", "!prompt", "!nofile" },
--- user/app/neovim/neovim.nix
+++ user/app/neovim/neovim.nix
@@ -47,6 +47,8 @@
       telescope-fzf-native-nvim
 
       kanagawa-nvim
+
+      better-escape-nvim
       which-key-nvim
       neoscroll-nvim
       neogit
@@ -75,19 +77,6 @@
 
       # Add custom config from overlay
       wlvs-nvim
-
-      {
-        plugin = better-escape-nvim;
-        type = "lua";
-        config = ''
-          require("better_escape").setup({
-            mapping = { "jk", "jj" }, -- a table with mappings to use
-            timeout = vim.o.timeoutlen, -- the time in which the keys must be hit in ms. Use option timeoutlen by default
-            clear_empty_lines = false, -- clear line after escaping if there is only whitespace
-            keys = "<Esc>", -- keys used for escaping, if it is a function will use the result everytime
-          })
-        '';
-      }
     ];
 
     extraLuaConfig = ''
