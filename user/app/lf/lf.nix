e18af9629e3b69c14b2e51b6eeb54d4da9d3ad3f
Wolves <cstingl@protonmail.com>
2023-12-15 10:47:57 -0500

    lf: Add ctpv basic image previewer/cleaner

--- user/app/lf/lf.nix
+++ user/app/lf/lf.nix
@@ -12,6 +12,8 @@
       drawbox = true;
       icons = true;
       ignorecase = true;
+      previewer = "${pkgs.ctpv}/bin/ctpv";
+      cleaner = "${pkgs.ctpv}/bin/ctpvclear";
     };
 
     commands = {
@@ -32,31 +34,5 @@
       ee = "editor-open";
       V = ''''$${pkgs.bat}/bin/bat --paging=always --theme=gruvbox "$f"'';
     };
-
-    # extraConfig = 
-    # let 
-    #   previewer = 
-    #     pkgs.writeShellScriptBin "pv.sh" ''
-    #     file=$1
-    #     w=$2
-    #     h=$3
-    #     x=$4
-    #     y=$5
-    #
-    #     if [[ "$( ${pkgs.file}/bin/file -Lb --mime-type "$file")" =~ ^image ]]; then
-    #         ${pkgs.kitty}/bin/kitty +kitten icat --silent --stdin no --transfer-mode file --place "''${w}x''${h}@''${x}x''${y}" "$file" < /dev/null > /dev/tty
-    #         exit 1
-    #     fi
-    #
-    #     ${pkgs.pistol}/bin/pistol "$file"
-    #   '';
-    #   cleaner = pkgs.writeShellScriptBin "clean.sh" ''
-    #     ${pkgs.kitty}/bin/kitty +kitten icat --clear --stdin no --silent --transfer-mode file < /dev/null > /dev/tty
-    #   '';
-    # in
-    # ''
-    #   set cleaner ${cleaner}/bin/clean.sh
-    #   set previewer ${previewer}/bin/pv.sh
-    # '';
   };
 }
