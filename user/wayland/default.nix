6d1c01352fc9ef137b215cbea32e6e888359f09f
Wolves <cstingl@protonmail.com>
2024-01-09 07:47:38 -0500

    hm: more config file refactor (swaylock)

--- user/wayland/default.nix
+++ user/wayland/default.nix
@@ -4,6 +4,7 @@
   imports = [
     (./. + "../../../user/wayland/wm"+("/"+wm))
     ./waybar
+    ./swaylock.nix
     ../app/terminal/foot.nix
   ];
 
@@ -34,45 +35,6 @@
   ];
 
   home.file.".wallpaper.png".source = ../../themes/aenami_escape.jpg;
-
-  home.file.".config/swaylock/config".text = ''
-    daemonize
-    image="$HOME/.wallpaper.png"
-    show-failed-attempts
-    clock
-    effect-blur=5x5
-    color=1f1d2e80
-    font="Product Sans"
-    indicator
-    indicator-radius=200
-    indicator-thickness=20
-    line-color=1f1d2e
-    ring-color=191724
-    inside-color=1f1d2e
-    key-hl-color=eb6f92
-    separator-color=00000000
-    text-color=e0def4
-    text-caps-lock-color=""
-    line-ver-color=eb6f92
-    ring-ver-color=eb6f92
-    inside-ver-color=1f1d2e
-    text-ver-color=e0def4
-    ring-wrong-color=31748f
-    text-wrong-color=31748f
-    inside-wrong-color=1f1d2e
-    inside-clear-color=1f1d2e
-    text-clear-color=e0def4
-    ring-clear-color=9ccfd8
-    line-clear-color=1f1d2e
-    line-wrong-color=1f1d2e
-    bs-hl-color=31748f
-    grace=0
-    grace-no-mouse
-    grace-no-touch
-    datestr=%m.%d
-    fade-in="0.1"
-    ignore-empty-password
-  '';
 
   home.file.".config/gtklock/style.css".text = ''
     window {
