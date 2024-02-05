b9fd0a9be6f7293062a04c618f1619a492cce0e4
Wolves <cstingl@protonmail.com>
2024-02-04 20:07:13 -0500

    tmux: add more tmux config

--- user/shell/tmux/default.nix
+++ user/shell/tmux/default.nix
@@ -5,10 +5,36 @@
     enable = true;
     shortcut = "a";
     baseIndex = 1;
+    keyMode = "vi";
+    terminal = "xterm-256color";
 
     plugins = with pkgs.tmuxPlugins; [
       better-mouse-mode
+      online-status
       pain-control
     ];
+
+    extraConfig = ''
+      set-option -g renumber-windows on
+
+      set -g message-style 'bg=default,fg=yellow,bold'
+
+      set -g set-titles on
+      set -g set-titles-string '#{window_index}.#{pane_index} ☞ #{pane_current_command}'
+
+      set -g prefix C-a
+      bind C-a send-prefix
+
+      unbind '"'
+      unbind %
+      unbind C-b
+      unbind -T copy-mode-vi MouseDragEnd1Pane
+      unbind -T copy-mode-vi y
+
+      # Copy mode bindings
+      bind-key -T copy-mode-vi y send-keys -X copy-pipe-and-cancel "wl-copy" \; display-message "highlighted selection copied to system clipboard"
+      bind-key -T copy-mode-vi MouseDragEnd1Pane send-keys -X copy-pipe-and-cancel "wl-copy" \; display-message "highlighted selection copied to system clipboard"
+      bind-key -T copy-mode-vi v send-keys -X begin-selection
+    '';
   };
 }
