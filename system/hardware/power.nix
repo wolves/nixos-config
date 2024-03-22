fc79fbc439d77ce5459e2998c6596974045bf19a
Wolves <cstingl@protonmail.com>
2024-03-22 14:07:30 -0400

    power: swap back to auto-cpufreq

--- system/hardware/power.nix
+++ system/hardware/power.nix
@@ -1,40 +1,40 @@
 { config, pkgs, ... }:
 
 {
-  # services.auto-cpufreq.enable = true;
-  # services.auto-cpufreq.settings = {
-  #   battery = {
-  #      governor = "powersave";
-  #      turbo = "never";
-  #   };
-  #   charger = {
-  #      governor = "performance";
-  #      turbo = "auto";
-  #   };
-  # };
-
-  services.tlp = {
-    enable = true;
-    settings = {
-      CPU_BOOST_ON_AC = 1;
-      CPU_BOOST_ON_BAT = 0;
-      CPU_SCALING_GOVERNOR_ON_AC = "performance";
-      CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
+  services.auto-cpufreq.enable = true;
+  services.auto-cpufreq.settings = {
+    battery = {
+       governor = "powersave";
+       turbo = "never";
+    };
+    charger = {
+       governor = "performance";
+       turbo = "auto";
     };
   };
 
-  services.power-profiles-daemon.enable = false;
-
-  powerManagement.powertop.enable = true;
-
+  # services.tlp = {
+  #   enable = false;
+  #   settings = {
+  #     CPU_BOOST_ON_AC = 1;
+  #     CPU_BOOST_ON_BAT = 0;
+  #     CPU_SCALING_GOVERNOR_ON_AC = "performance";
+  #     CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
+  #   };
+  # };
+  #
+  # services.power-profiles-daemon.enable = false;
+  #
+  # powerManagement.powertop.enable = false;
+  #
   services.thermald.enable = true;
-
-  #services.tlp = {
-  #  enable = true;
-  #  settings = {
-  #    CPU_SCALING_GOVERNOR_ON_AC = "performance";
-  #    CPU_SCALING_GOVERNOR_ON_BAT = "schedutil";
-
+  #
+  # #services.tlp = {
+  # #  enable = true;
+  # #  settings = {
+  # #    CPU_SCALING_GOVERNOR_ON_AC = "performance";
+  # #    CPU_SCALING_GOVERNOR_ON_BAT = "schedutil";
+  #
   #    CPU_ENERGY_PERF_POLICY_ON_BAT = "power";
   #    CPU_ENERGY_PERF_POLICY_ON_AC = "performance";
 
