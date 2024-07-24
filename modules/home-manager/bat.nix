c4f55ed8314ec054337d86f1e21ab3d86c99a03f
wolves <cstingl@protonmail.com>
2024-07-24 10:26:35 -0400

    bat: update bat theme to fetch kanagawa.tmTheme from github

--- modules/home-manager/bat.nix
+++ modules/home-manager/bat.nix
@@ -1,9 +1,21 @@
 {
-  programs = {
-    bat = {
-      enable = true;
-      config = {
-        theme = "base16-256";
+  pkgs,
+  ...
+}:{
+  programs.bat = {
+    enable = true;
+    config = { theme = "kanagawa"; };
+    themes = {
+      kanagawa = {
+        src =
+          pkgs.fetchFromGitHub
+          {
+            owner = "rebelot";
+            repo = "kanagawa.nvim";
+            rev = "e5f7b8a804360f0a48e40d0083a97193ee4fcc87";
+            sha256 = "sha256-FnwqqF/jtCgfmjIIR70xx8kL5oAqonrbDEGNw0sixoA=";
+          };
+        file = "extras/kanagawa.tmTheme";
       };
     };
   };
