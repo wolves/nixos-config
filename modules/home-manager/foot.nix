fbf0bfe83ff5bda7cf4ccefe1879c7616ffe1464
wolves <cstingl@protonmail.com>
2024-08-06 14:26:36 -0400

    font: Trying out GeistMono font

--- modules/home-manager/foot.nix
+++ modules/home-manager/foot.nix
@@ -1,6 +1,8 @@
-{ config, pkgs, ... }:
-
 {
+  config,
+  pkgs,
+  ...
+}: {
   home.packages = with pkgs; [
     foot
   ];
@@ -8,6 +10,8 @@
   programs.foot.enable = true;
   programs.foot.settings = {
     main = {
+      # font = "GeistMono Nerd Font:size=9.5:fontfeatures=calt:fontfeatures=dlig:fontfeatures=liga";
+      # box-drawings-uses-font-glyphs = "yes";
       font = "Iosevka:size=10";
       font-size-adjustment = 1;
       dpi-aware = "no";
@@ -32,17 +36,16 @@
       regular5 = "957fb8";
       regular6 = "6a9589";
       regular7 = "c8c093";
-      bright0  = "727169";
-      bright1  = "e82424";
-      bright2  = "98bb6c";
-      bright3  = "e6c384";
-      bright4  = "7fb4ca";
-      bright5  = "938aa9";
-      bright6  = "7aa89f";
-      bright7  = "dcd7ba";
-      "16"       = "ffa066";
-      "17"       = "ff5d62";
+      bright0 = "727169";
+      bright1 = "e82424";
+      bright2 = "98bb6c";
+      bright3 = "e6c384";
+      bright4 = "7fb4ca";
+      bright5 = "938aa9";
+      bright6 = "7aa89f";
+      bright7 = "dcd7ba";
+      "16" = "ffa066";
+      "17" = "ff5d62";
     };
   };
 }
-
